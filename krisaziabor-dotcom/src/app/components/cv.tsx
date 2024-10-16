272d2561953edf5e769ef6019dc73646bcfc6e1b
kris aziabor <krisaaziabor@gmail.com>
2024-10-16 02:24:58 -0400

    formatting using prettier

--- krisaziabor-dotcom/src/app/components/cv.tsx
+++ krisaziabor-dotcom/src/app/components/cv.tsx
@@ -1,27 +1,27 @@
 export default function Hero() {
-    return (
-        <div className="min-h-screen bg-background flex items-center sm:items-end justify-center sm:justify-start p-8 sm:p-20 font-[family-name:var(--font-abc-diatype)]">
-          <main className="text-base text-left text-foreground mx-auto sm:mx-0 max-w-[90%] sm:max-w-none">
-            <ol>
-              <li className="mb-4">
-                Any ambition to create timeless work renders it lifeless.
-                <br />
-                Its morality and rebirth will always be the most beautiful.
-              </li>
-    
-              <li className="mb-4">Kris Aziabor Dot Com, coming back soon &lt;3</li>
-    
-              <li>
-                <a
-                  href="http://krisaziabor.com/Design%20at%20Yale.pdf" 
-                  target="_blank"
-                  className="hover:text-orange-400"
-                >
-                  View my portfolio -&gt;
-                </a>
-              </li>
-            </ol>
-          </main>
-        </div>
-      );
+  return (
+    <div className="min-h-screen bg-background flex items-center sm:items-end justify-center sm:justify-start p-8 sm:p-20 font-[family-name:var(--font-abc-diatype)]">
+      <main className="text-base text-left text-foreground mx-auto sm:mx-0 max-w-[90%] sm:max-w-none">
+        <ol>
+          <li className="mb-4">
+            Any ambition to create timeless work renders it lifeless.
+            <br />
+            Its morality and rebirth will always be the most beautiful.
+          </li>
+
+          <li className="mb-4">Kris Aziabor Dot Com, coming back soon &lt;3</li>
+
+          <li>
+            <a
+              href="http://krisaziabor.com/Design%20at%20Yale.pdf"
+              target="_blank"
+              className="hover:text-orange-400"
+            >
+              View my portfolio -&gt;
+            </a>
+          </li>
+        </ol>
+      </main>
+    </div>
+  );
 }
