3f4a078ecdeb6d49af67441889e1921ed9ed4885
kris aziabor <krisaaziabor@gmail.com>
2024-10-29 15:41:01 -0400

    fixed to items-center sm:items-end

--- krisaziabor-dotcom/src/app/page.tsx
+++ krisaziabor-dotcom/src/app/page.tsx
@@ -3,7 +3,7 @@ import Bio from "./components/bio";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background grid grid-cols-1 sm:grid-cols-2 items-end p-8 sm:p-20 font-[family-name:var(--font-abc-diatype)]">
+    <div className="min-h-screen bg-background grid grid-cols-1 sm:grid-cols-2 items-center sm:items-end p-8 sm:p-20 font-[family-name:var(--font-abc-diatype)]">
       <Bio />
       {/* <CV /> */}
     </div>
