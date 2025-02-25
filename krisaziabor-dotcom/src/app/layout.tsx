5883ebca748617a849972731e1a6bba6ff31a690
kris aziabor <krisaaziabor@gmail.com>
2025-02-25 09:54:53 -0500

    new ico and preview png

--- krisaziabor-dotcom/src/app/layout.tsx
+++ krisaziabor-dotcom/src/app/layout.tsx
@@ -19,13 +19,39 @@ const boldabcdiatype = localFont({
 });
 
 
-
 export const metadata: Metadata = {
   title: "Kris Aziabor",
-  description: "Creating striking, accessible designs with unconventional & powerful software.",
+  description:
+    "Creating striking, accessible designs with unconventional & powerful software.",
   icons: "icon.ico",
-};
 
+  // This is where you can add your Open Graph details:
+  openGraph: {
+    title: "Kris Aziabor",
+    description:
+      "Creating striking, accessible designs with unconventional & powerful software.",
+    url: "https://krisaziabor.com",
+    siteName: "Kris Aziabor",
+    images: [
+      {
+        url: "/preview.png",
+        width: 1200,
+        height: 670,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  // And here are Twitter-specific tags (many platforms also read them):
+  twitter: {
+    card: "summary_large_image",
+    title: "Kris Aziabor",
+    description:
+      "Creating striking, accessible designs with unconventional & powerful software.",
+    images: ["/preview.png"],
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
