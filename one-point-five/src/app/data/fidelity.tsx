ba49ad338f0a2f3ac51674d833d79fdc627f5d66
kris aziabor <krisaaziabor@gmail.com>
2025-03-11 23:47:13 -0400

    fixed writing fidelity

--- one-point-five/src/app/data/fidelity.tsx
+++ one-point-five/src/app/data/fidelity.tsx
@@ -75,21 +75,21 @@ const fidelity = {
   After only a week from receiving the feasibility study project, Shash, Jenny, and I looked at each other and knew we wanted to get our hands dirtier. As we were brought in to be full-stack software engineering interns, we craved creating a full-stack project. After attending a few days of office hours, we knew the problem we wanted to solve. We put together a slide deck, requested to meet with our bosses, and proposed our idea: an in-house web client bringing together multiple file protocols under a familiar UI. We knew that Fidelity employees were struggling to familiarize themselves with the nuances of each protocol’s application (SFTP, Ad Hoc, HTTPS, etc) and our coworkers in the MFT team were spending hours on call to support them. Bringing as many as the protocols together under one platform was the central goal. Crucially, by placing these powerful softwares all within an email user experience that is widely familiar, we would elevate the productivity of the company. We used JScape’s API for all the central functionality and utilized a Python Django stack with Tailwind for seamless styling and backend management. By using the Figma handoff features, I was able to dramatically cut down time for our team between the design and implementation stages.
 
   ![Fidelity Client mockup](/Fidelity.jpg)
-  ##### A resketch of the mockup created for the Fidelity Client MVP. Centered around a minimalistic and easy-to-use design, the client mimics email for less intuitive protocols like SFTP and AS2.
+  ##### A rough resketch of the mockup created for the Fidelity Client MVP. Centered around a minimalistic and easy-to-use design, the client mimics email for less intuitive protocols like SFTP and AS2.
   
-  Ultimately, we were able to make a full Figma prototype and break ground on the coded MVP. We would have liked to achieve more with the project, but we left our work in good hands for the rest of the team to build from our foundation.
+  Ultimately, we were able to make a full Figma prototype and break ground on the coded MVP. While we would have liked to achieve more with the project, we established a meaningful foundation and left our work in the good hands of the rest our the team.
 
   ## REFLECTION
 
   The two plus months I spent in the Merrimack office surprised me in almost every way. 
 
-  I gained a lot of perspective on what it means to work in technology within the financial world. My co-interns and I expected to be done with the case study in a matter of weeks but we soon learned that this was naive. We needed to get past company security checkpoints, troubleshoot with outside companies, and do lots and lots of waiting. We were only able to wrap things up in our penultimate week and were not able to finalize as much of the MVP as we would have liked. This is not a damning indictment on us or the company but simply just the realities of the job – your desire to produce may be much quicker than your ability to get the job done.
+  I gained a lot of perspective on what it means to work on software within the financial world. My co-interns and I expected to be done with the feasibility study in a matter of weeks. To our surprise, we soon learned that this was naive. We needed to get past company security checkpoints, troubleshoot with outside firms, and generally, do lots and lots of waiting. We were only able to wrap things up in our penultimate week and were not able to finalize as much of the MVP as we would have liked. This is not a damning indictment on us or the company but simply just the realities of the job – outside factors may bring your most menial tasks to a screeching halt.
 
   These slight disappointments exist, but they do not change the fact that the internship was a wonderful experience.
 
-  Beyond my work as a full-stack software engineer, I had become the team’s scrum master, crafted the slides decks used in senior member presentations, and  conducted research for a product prototype. 
+  Beyond my work as a full-stack software engineer, I had become the team’s defacto scrum master and product designer. I crafted the Figma prototype central to our MVP, curated the slides decks used in senior member presentations, and conducted research for a product prototype. 
 
-  In just ten weeks, I dove deep into the world of MFT (managed file transfer, a concept I had never heard of prior), presented my work to various groups of senior leadership, did work befitting of a product designer, researcher, and manager, and even sprinkled in some intense pickleball playing (me and Melissa still have our undefeated record btw).
+  In just ten weeks, I dove deep into the world of MFT, presented my work to various groups of senior leadership, did work befitting of a several product roles, and even sprinkled in some intense pickleball playing (me and Melissa still have our undefeated record btw {Shash and Jackson that one "win" doesn't count}).
 
   It was a wonderful second internship and I will forever be grateful for all who made it possible \<3
 
